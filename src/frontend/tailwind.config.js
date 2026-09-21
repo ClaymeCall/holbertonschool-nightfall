63c7bba6f54fb92327b683c3080fd106324e5112
Clement Callejon <clement.callejon@tutanota.com>
2026-09-21 21:23:35 +0200

    fix: migrate to Vite and correctly expose container for dev work via volum

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -14,4 +14,3 @@ module.exports = {
   },
   plugins: [],
 }
-
